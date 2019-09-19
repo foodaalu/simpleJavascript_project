4db664429bdbc45375d320e34cded65e4de588b4
sarojghising <sarojghising13@gmail.com>
2019-09-19 19:35:36 +0545

    first_commit

---
+++ basic_javascript_project/hex_color_change/main.js
@@ -0,0 +1,7 @@
+const hexnumber = [0,1,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const btn  = document.querySelector('.hexBtn');
+const body = document.querySelector('body');
+//console.log(hexnumber,btn,body);
+
+
+
