2f309782e85376efa43993e4eb3038af4ef5dee1
sarojghising <sarojghising13@gmail.com>
2019-09-20 07:19:25 +0545

    first  commit

--- basic_javascript_project/hex_color_change/main.js
+++ basic_javascript_project/hex_color_change/main.js
@@ -1,7 +1,17 @@
-const hexnumber = [0,1,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-const btn  = document.querySelector('.hexBtn');
+const randomHexColor = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const btnclick = document.querySelector('.hexBtn');
 const body = document.querySelector('body');
-//console.log(hexnumber,btn,body);
+const textMessage = document.querySelector('.hex');
 
 
 
+btnclick.addEventListener('click',GenerateNumber);
+function GenerateNumber(){
+    let hex = '#';
+    for(let i=0;i<6;i++){
+        let random = Math.floor(Math.random() * randomHexColor.length);
+        hex += randomHexColor[random];
+    }
+    body.style.backgroundColor = hex;
+    textMessage.innerHTML = hex;
+}
