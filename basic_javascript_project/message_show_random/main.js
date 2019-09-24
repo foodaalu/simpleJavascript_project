b10e14f4a3bfb55d22f6a2ce27f9aefbc18b653e
sarojghising <sarojghising13@gmail.com>
2019-09-24 21:24:59 +0545

    first commit

---
+++ basic_javascript_project/message_show_random/main.js
@@ -0,0 +1,40 @@
+ const quotes =[
+ {
+      name:'Stephen King',
+      quote: 'Get busy living or get busy dying.'
+ },
+ {
+    name:'hello wrold',
+    quote: 'It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happened to things.'
+},
+{
+    name:'Stephen King',
+    quote: 'Get busy living or get busy dying.'
+},
+{
+    name:'Stephen King',
+    quote: 'Get busy living or get busy dying.'
+},
+{
+    name:'Stephen King',
+    quote: 'Get busy living or get busy dying.'
+},
+ ]
+const quoteBtn = document.querySelector('#quoteBtn');
+const quoteAuthor = document.querySelector('#quoteAuthor');
+const quote = document.querySelector('#quote');
+quoteBtn.addEventListener('click',displayQuote);
+function displayQuote(){
+            let number = Math.floor(Math.random() * quotes.length);
+            // console.log(number);
+            quoteAuthor.innerHTML = quotes[number].name;
+            quote.innerHTML = quotes[number].quote;
+
+
+            
+}
+ //console.log(quotes);
+ 
+
+
+
